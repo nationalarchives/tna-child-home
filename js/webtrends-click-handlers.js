3de1921bf418124510a839c69fb973c67b77d5d2
Punal Chotrani <punalsc@gmail.com>
2016-06-30 17:19:18 +0100

    further clean up

--- js/webtrends-click-handlers.js
+++ js/webtrends-click-handlers.js
@@ -1,26 +1,16 @@
 window.addEventListener("load", function () {
 
-    if (!$) {
-        var jq = document.createElement('script');
-        jq.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js';
-        document.getElementsByTagName('body')[0].appendChild(jq);
+    if (typeof $ === 'function') {
+        $('a', '.mega-menu').on('click', function (e) {
+            if (typeof dcsMultiTrack == 'function') {
+                var text = $(e.target).text();
+                dcsMultiTrack(
+                    'DCS.dcsuri',
+                    '/menu/' + text,
+                    'WT.ti',
+                    'Menu - ' + text
+                )
+            }
+        })
     }
-
-    $('a', '.mega-menu').on('click', function (e) {
-        if (typeof dcsMultiTrack == 'function') {
-            var text = $(e.target).text();
-            dcsMultiTrack(
-                'DCS.dcsuri',
-                '/menu/' + text,
-                'WT.ti',
-                'Menu - ' + text
-            )
-        }
-    })
 });
-
-/*
-dcsMultiTrack = function () {
-    console.log.apply(console, arguments);
-};
-*/
