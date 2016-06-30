6d71ffc334a0173daffa05e9cba25ded843e0449
Punal Chotrani <punalsc@gmail.com>
2016-06-30 17:03:42 +0100

    Cleaned up code.

--- js/webtrends-click-handlers.js
+++ js/webtrends-click-handlers.js
@@ -1,41 +1,26 @@
-if (!window.jQuery) {
-    var jq = document.createElement('script'); jq.type = 'text/javascript';
-    jq.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js';
-    document.getElementsByTagName('body')[0].appendChild(jq);
+window.addEventListener("load", function () {
 
-    window.addEventListener("load", function () {
-        $('a', '.mega-menu').on('click', function (e) {
-            if (typeof dcsMultiTrack == 'function' && !!dcsMultiTrack && window.jQuery) {
-                var text = $(e.target).text();
-                dcsMultiTrack(
-                    'DCS.dcsuri',
-                    '/menu/' + text,
-                    'WT.ti',
-                    'Menu - ' + text
-                )
-            }
-        })
-    });
-}
-else {
-    window.addEventListener("load", function () {
-        $('a', '.mega-menu').on('click', function (e) {
-            if (typeof dcsMultiTrack == 'function' && !!dcsMultiTrack && window.jQuery) {
-                var text = $(e.target).text();
-                dcsMultiTrack(
-                    'DCS.dcsuri',
-                    '/menu/' + text,
-                    'WT.ti',
-                    'Menu - ' + text
-                )
-            }
-        })
-    });
-}
+    if (!$) {
+        var jq = document.createElement('script');
+        jq.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js';
+        document.getElementsByTagName('body')[0].appendChild(jq);
+    }
+
+    $('a', '.mega-menu').on('click', function (e) {
+        if (typeof dcsMultiTrack == 'function') {
+            var text = $(e.target).text();
+            dcsMultiTrack(
+                'DCS.dcsuri',
+                '/menu/' + text,
+                'WT.ti',
+                'Menu - ' + text
+            )
+        }
+    })
+});
 
 /*
 dcsMultiTrack = function () {
     console.log.apply(console, arguments);
 };
 */
-
