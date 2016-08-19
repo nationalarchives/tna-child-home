766dfb006adb8ce091001a4d0d4dcfc66671b9c3
Punal Chotrani <punalsc@gmail.com>
2016-08-19 16:06:26 +0100

    Added class to main <ul>.

--- navigation.php
+++ navigation.php
@@ -13,7 +13,7 @@ wp_nav_menu(
 	array(  'menu'              =>  'mega-menu',
 			'sort_column'       =>  'menu_order',
 			'container'         =>  false,
-			'items_wrap'        =>  '<ul>%3$s</ul>'
+			'items_wrap'        =>  '<ul class="main-ul">%3$s</ul>'
 	)
 );
 echo '</div>';
