e62cf7e46342d9285a2ebf8080d1bf4a7339e829
Chris Bishop <domingobishop@gmail.com>
2017-05-09 15:25:39 +0100

    phpunit setup

---
+++ tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+// Enable Composer autoloader
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+// Load the required form functions
+// require dirname(__DIR__) . '/functions-home.php';
