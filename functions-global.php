a0e09f0c0fc2543e54da9572118b96e5d88621aa
Chris Bishop <domingobishop@gmail.com>
2017-05-18 15:02:59 +0100

    refactored functions.php

---
+++ functions-global.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Global functions
+ */
+
+// For breadcrumbs and URLs
+// Edit as required
+function tnatheme_globals() {
+	global $pre_path;
+	global $pre_crumbs;
+	// If internal TNA
+	if (substr($_SERVER['REMOTE_ADDR'], 0, 3) === '10.') {
+		$pre_path = '';
+		$pre_crumbs = '';
+		// If external TNA
+	} else {
+		$pre_crumbs = '';
+		$pre_path = '';
+	}
+}
+// If web development machine
+if ( $_SERVER['SERVER_ADDR'] !== $_SERVER['REMOTE_ADDR'] ) {
+	tnatheme_globals();
+} else {
+	$pre_path = '';
+	$pre_crumbs = '';
+}
+
+// Dequeue parent styles for re-enqueuing in the correct order
+function dequeue_parent_style() {
+	wp_dequeue_style('tna-styles');
+	wp_deregister_style('tna-styles');
+}
+
+// Enqueue styles in correct order
+function tna_child_styles() {
+	wp_register_style( 'tna-parent-styles', get_template_directory_uri() . '/css/base-sass.css.min', array(), EDD_VERSION, 'all' );
+	wp_register_style( 'tna-child-styles', get_stylesheet_directory_uri() . '/css/home-sass.css.min', array(), '0.1', 'all' );
+	wp_enqueue_style( 'tna-parent-styles' );
+	wp_enqueue_style( 'tna-child-styles' );
+}
+
+//Filters out ids and classes from li and ul elements
+function attributes_filter($var) {
+	return is_array($var) ? array_intersect($var, array('current-menu-item')) : '';
+}
