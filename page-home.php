65d2a37a6dcb66d7de6c6445d16e5b1d1aa9cfca
Chris Bishop <domingobishop@gmail.com>
2017-05-26 11:46:11 +0100

    remove override var

--- page-home.php
+++ page-home.php
@@ -25,8 +25,8 @@ get_header(); ?>
 				for ( $i=1 ; $i<=6 ; $i++ ) {
 
 					$url        = get_post_meta( $post->ID, 'home_card_url_'.$i, true );
-					$title      = get_post_meta( $post->ID, 'home_card_title_'.$i, true );
-					$image      = get_post_meta( $post->ID, 'home_card_img_'.$i, true );
+					// $title      = get_post_meta( $post->ID, 'home_card_title_'.$i, true );
+					// $image      = get_post_meta( $post->ID, 'home_card_img_'.$i, true );
 					$expire     = get_post_meta( $post->ID, 'home_card_expire_'.$i, true );
 					$fallback   = get_post_meta( $post->ID, 'home_card_fallback_'.$i, true );
 
