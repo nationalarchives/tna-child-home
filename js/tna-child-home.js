2b7fabf5c09b7d5fb07369571cadff53a8b8cb53
Chris Bishop <domingobishop@gmail.com>
2017-05-31 09:48:55 +0100

    js to manage cards if not 3 or 6

--- js/tna-child-home.js
+++ js/tna-child-home.js
@@ -15,7 +15,7 @@ jQuery(document).ready(function() {
         }
     }
 
-    if ($count > 3) {
+    if ( $count > 3 && $count < 6 ) {
         var elements = document.getElementsByClassName("col-card");
 
         for (var i = 3; i < elements.length; i++) {
