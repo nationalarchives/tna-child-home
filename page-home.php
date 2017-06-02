38624003fde54e1ff58988e56bd486a30831b405
Chris Bishop <domingobishop@gmail.com>
2017-06-02 15:01:20 +0100

    homepage partials

--- page-home.php
+++ page-home.php
@@ -8,55 +8,14 @@ get_header(); ?>
 
 	<?php get_template_part( 'partials/masthead' ); ?>
 
-	<?php
-		global $post;
-		$expire     = get_post_meta( $post->ID, 'home_banner_expire', true );
-		$status     = get_post_meta( $post->ID, 'home_banner_status', true );
-		$image      = get_post_meta( $post->ID, 'home_banner_img', true );
-		$title      = get_post_meta( $post->ID, 'home_banner_title', true );
-		$excerpt    = get_post_meta( $post->ID, 'home_banner_excerpt', true );
-		$url        = get_post_meta( $post->ID, 'home_banner_url', true );
-		$button     = get_post_meta( $post->ID, 'home_banner_btn', true );
+	<?php get_template_part( 'partials/hero', 'banner' ); ?>
 
-		echo home_banner( $expire, $status, $image, $title, $excerpt, $url, $button );
-	?>
 	<div class="container">
 		<div class="row">
 			<div class="cards-wrapper equal-heights-flex-box clearfix">
-				<?php
 
-				for ( $i=1 ; $i<=6 ; $i++ ) {
+				<?php get_template_part( 'partials/cards' ); ?>
 
-					$url        = get_post_meta( $post->ID, 'home_card_url_'.$i, true );
-					// $title      = get_post_meta( $post->ID, 'home_card_title_'.$i, true );
-					// $image      = get_post_meta( $post->ID, 'home_card_img_'.$i, true );
-					$expire     = get_post_meta( $post->ID, 'home_card_expire_'.$i, true );
-					$fallback   = get_post_meta( $post->ID, 'home_card_fallback_'.$i, true );
-
-					$title = '';
-					$image = '';
-
-					$transient  = get_transient( 'homepage_cards_html'.$i );
-
-					if( !empty( $transient ) && is_card_active( $expire ) ) {
-
-						echo $transient ;
-
-					} elseif ( is_card_active( $expire ) ) {
-
-						$html = get_content_and_display_card( $i, $url, $title, $image );
-
-						set_transient( 'homepage_cards_html'.$i, $html, MONTH_IN_SECONDS );
-
-						echo $html;
-
-					} else {
-
-						echo card_fallback( $fallback, $i );
-
-					}
-				}
-				?>
 			</div>
 		</div>
 	</div>
