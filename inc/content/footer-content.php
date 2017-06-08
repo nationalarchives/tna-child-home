0091ca9ce648cd511ce701b3acd2b5ad5c949153
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2017-06-08 12:08:50 +0100

    1. Added opening times inside footer 2. Added follow us icons inside footer 3. Addapted the grid system and changed the position of the columns for mobile and devices 4. Added footer stylesheet 5. Tested in Chrome, Safari and Firefox

---
+++ inc/content/footer-content.php
@@ -0,0 +1,156 @@
+<div class="row">
+    <div class="footer-col col-xs-12 col-sm-12 col-md-3 col-lg-3">
+        <div class="row">
+            <div class="footer-col col-xs-12 col-sm-6 col-md-12 col-lg-12">
+                <address>
+                    The National Archives<br/>
+                    Kew, Richmond, Surrey,<br/>
+                    TW9 4DU
+                </address>
+                Tel: <a class="tel" href="tel:+4402088763444">+44 (0) 20 8876 3444</a>
+            </div>
+            <div class="footer-col col-xs-12 col-sm-6 col-md-12 col-lg-12">
+                <h4>Standard opening times</h4>
+                <div class="opening-times">
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td>Monday</td>
+                            <td>Closed</td>
+                        </tr>
+                        <tr>
+                            <td>Tuesday</td>
+                            <td>09:00 – 19:00</td>
+                        </tr>
+                        <tr>
+                            <td>Wednesday</td>
+                            <td>09:00 – 17:00</td>
+                        </tr>
+                        <tr>
+                            <td>Thursday</td>
+                            <td>09:00 – 19:00</td>
+                        </tr>
+                        <tr>
+                            <td>Friday</td>
+                            <td>09:00 – 17:00</td>
+                        </tr>
+                        <tr>
+                            <td>Saturday</td>
+                            <td>09:00 – 17:00</td>
+                        </tr>
+                        <tr>
+                            <td>Sunday</td>
+                            <td>Closed</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <a href="/about/visit-us/opening-times/">Full opening times</a>
+            </div>
+        </div>
+    </div>
+    <div class="footer-col col-xs-12 col-sm-12 col-md-6">
+        <div class="row">
+            <div class="footer-col col-xs-12 col-sm-6 col-md-6">
+                <h4>Find out more</h4>
+                <ul>
+                    <li><a href="/contact-us/">Contact us</a></li>
+                    <li><a href="/about/press-room/">Press room</a></li>
+                    <li><a href="/about/jobs/">Jobs and careers</a></li>
+                    <li><a href="/about/get-involved/friends-of-the-national-archives/">Friends of The National
+                            Archives</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-col col-xs-12 col-sm-6 col-md-6">
+                <h4>Websites</h4>
+                <ul>
+                    <li><a href="http://blog.nationalarchives.gov.uk/">Blog</a></li>
+                    <li><a href="http://media.nationalarchives.gov.uk/">Podcasts and videos</a></li>
+                    <li><a href="http://bookshop.nationalarchives.gov.uk/">Bookshop</a></li>
+                    <li><a href="https://images.nationalarchives.gov.uk/">Image library</a></li>
+                    <li><a href="/webarchive/">UK Government Web Archive</a></li>
+                    <li><a href="http://www.legislation.gov.uk/" target="_blank">Legislation.gov.uk</a></li>
+                    <li><a href="https://www.thegazette.co.uk/" target="_blank">The Gazette</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="footer-col col-xs-12 col-sm-6 col-md-6">
+                <h4>Site help</h4>
+                <ul>
+                    <li><a href="/help/">Help</a></li>
+                    <li><a href="/help/faqs.htm">FAQs</a></li>
+                    <li><a href="/help/atoz.htm">Website A-Z index</a></li>
+                    <li><a href="/help/accessibility.htm">Accessibility</a></li>
+                    <li><a href="/help/sitemap.htm">Site map</a></li>
+                </ul>
+            </div>
+            <div class="footer-col col-xs-12 col-sm-6 col-md-6">
+                <h4>Legal</h4>
+                <ul>
+                    <li><a href="/legal/">Terms of use</a></li>
+                    <li><a href="/legal/privacy.htm">Privacy policy</a></li>
+                    <li><a href="/legal/cookies.htm">Cookies</a></li>
+                    <li><a href="/about/freedom-of-information/">Freedom of Information</a></li>
+                    <li><a href="/about/our-role/transparency/">Transparency</a></li>
+                    <li><a href="/legal/our-fees.htm">Our fees</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-col col-xs-12 col-sm-12 col-md-3">
+        <div class="row">
+            <div class="footer-col col-xs-12 col-sm-6 col-md-12">
+                <h4>Follow us</h4>
+                <div class="follow-us">
+                    <a href="https://twitter.com/@UKNatArchives" title="Follow us on Twitter">
+                        <img src="<?php echo get_stylesheet_directory_uri() ?>/img/social/twitter.png"
+                             alt="Twitter logo"></a>
+                    <a href="https://www.youtube.com/c/TheNationalArchivesUK">
+                        <img src="<?php echo get_stylesheet_directory_uri() ?>/img/social/youtube-play.png" alt="YouTube logo"></a>
+                    <a href="https://www.flickr.com/photos/nationalarchives">
+                        <img src="<?php echo get_stylesheet_directory_uri() ?>/img/social/flickr.png" alt="Flikr logo"></a>
+                    <a href="https://www.facebook.com/TheNationalArchives">
+                        <img src="<?php echo get_stylesheet_directory_uri() ?>/img/social/facebook.png"
+                             alt="Facebook logo"></a>
+                    <a href="http://www.nationalarchives.gov.uk/rss/">
+                        <img src="<?php echo get_stylesheet_directory_uri() ?>/img/social/rss.png" alt="RSS logo"></a>
+                </div>
+            </div>
+
+            <div class="footer-col col-xs-12 col-sm-6 col-md-12">
+                <h4>Featured Flickr image</h4>
+                <p id="flickr-caption">West End London street scene, by Grace Golden (<a
+                            href="https://www.flickr.com/photos/nationalarchives/3047452739/"
+                            title="External website - opens in new window" target="_blank">INF 3/1738</a>)</p>
+                <p id="flickr-image">
+                    <a target="_blank" title="External website - opens in a new window"
+                       href="https://www.flickr.com/photos/nationalarchives/3047452739/">
+                        <img class="img-responsive"
+                             src="https://www.nationalarchives.gov.uk/images/global/inf-3-1738-grace-golden.jpg"
+                             alt="West End London street scene, by Grace Golden">
+                    </a>
+                </p>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row footer-base">
+    <div class="col-xs-12 col-sm-9 col-md-9 ogl">
+        <a title="External website - opens in a new window"
+           href="http://www.nationalarchives.gov.uk/doc/open-government-licence/"> <img
+                    alt="Open Government License logo"
+                    src="<?php echo str_replace(home_url(), '', get_template_directory_uri()); ?>/img/logo-ogl.png">
+        </a>
+        <p>All content is available under the <a href="http://www.nationalarchives.gov.uk/doc/open-government-licence/">Open
+                Government Licence v3.0</a>, <br>except where otherwise stated </p>
+    </div>
+    <div class="col-xs-12 col-sm-3 col-md-3 text-right">
+        <a title="External website - opens in a new window" href="http://gov.uk/" target="_blank"> <img
+                    alt="GOV.UK logo"
+                    src="<?php echo str_replace(home_url(), '', get_template_directory_uri()); ?>/img/gov-uk.png"> </a>
+    </div>
+</div>
