1e910c04433834e7cbda92b08ff36b1c5eeb821b
Chris Bishop <domingobishop@gmail.com>
2017-06-22 16:39:01 +0100

    set cookie when alert closed

--- js/tna-child-home.js
+++ js/tna-child-home.js
@@ -22,8 +22,31 @@ jQuery(document).ready(function() {
         }
     }
 
+    var tnaSetThisCookie = function (name, days) {
+        var d = new Date();
+        d.setTime(d.getTime() + 1000 * 60 * 60 * 24 * days);
+        document.cookie = name + "=true;path=/;expires=" + d.toGMTString() + ';';
+    };
+
+    var tnaCheckForThisCookie = function (name) {
+        if (document.cookie.indexOf(name) === -1) {
+            return false;
+        } else {
+            return true;
+        }
+    };
+
+    $(function () {
+        if (!tnaCheckForThisCookie("dontShowHomeAlertTest3")) {
+            $("#home_alert").show();
+        }
+    });
+
     $("#alert_close_button").click(function () {
-        $("#home_alert").css("display", "none");
+
+        tnaSetThisCookie('dontShowHomeAlertTest3', 1);
+
+        $("#home_alert").hide();
     });
 
 });
