bdd391a22b376bf366d1905318702b5484cc17c6
Chris Bishop <domingobishop@gmail.com>
2017-08-02 16:47:56 +0100

    removed redundant css and divs

--- partials/masthead.php
+++ partials/masthead.php
@@ -13,37 +13,29 @@ if ( get_option('home_masthead_btn_text_1') ) {
 	} ?>
 
 	<div class="masthead-wrapper" <?php echo $masthead_image; ?>>
-		<div class="masthead-overlay">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-6 col-sm-6">
+					<div class="col-md-12">
 						<h1><?php echo get_option('home_masthead_desc'); ?></h1>
 					</div>
-					<div class="col-md-6 col-sm-6">
-						<div class="opening-times">
-							<?php echo display_tna_opening_status( date('Y-m-d'), date('l'), get_openingtimes_overrides() ); ?><br>
-							<a href="http://www.nationalarchives.gov.uk/about/visit-us/">Plan your visit</a>
-						</div>
+					<div class="opening-times">
+						<?php echo display_tna_opening_status( date('Y-m-d'), date('l'), get_openingtimes_overrides() ); ?><br>
+						<a href="http://www.nationalarchives.gov.uk/about/visit-us/">Plan your visit</a>
+					</div>
+					<div class="masthead-buttons">
+						<?php for ( $i=1 ; $i<=3 ; $i++ ) {
+
+							$btn_text = esc_attr( get_option('home_masthead_btn_text_'.$i) );
+							$btn_url = esc_attr( get_option('home_masthead_btn_url_'.$i) );
+
+							?>
+								<a class="button" role="button" href="<?php echo $btn_url ?>">
+									<span><?php echo $btn_text ?></span>
+								</a>
+						<?php } ?>
 					</div>
 				</div>
-				<div class="row">
-					<?php for ( $i=1 ; $i<=3 ; $i++ ) {
-
-						$btn_text = esc_attr( get_option('home_masthead_btn_text_'.$i) );
-						$btn_url = esc_attr( get_option('home_masthead_btn_url_'.$i) );
-
-						?>
-
-						<div class="col-md-4 col-sm-4">
-							<a class="button" role="button" href="<?php echo $btn_url ?>">
-								<span><?php echo $btn_text ?></span>
-							</a>
-						</div>
-
-					<?php } ?>
-				</div>
 			</div>
-		</div>
 	</div>
 
 <?php }
