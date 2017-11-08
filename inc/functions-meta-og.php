b7d995603d8acd9774cd518f13ec36ad5a157692
Chris Bishop <domingobishop@gmail.com>
2017-11-08 14:49:44 +0000

    expire date maintained

--- inc/functions-meta-og.php
+++ inc/functions-meta-og.php
@@ -23,16 +23,17 @@ function check_result( $result ) {
  * @since 1.0
  *
  * @param string $url
+ *
  * @return string
  */
 function get_html_content( $url ) {
 
-	if ( !class_exists('WP_Http') ) {
-		include_once( ABSPATH . WPINC . '/class-http.php');
+	if ( ! class_exists( 'WP_Http' ) ) {
+		include_once( ABSPATH . WPINC . '/class-http.php' );
 	}
 
 	$request = new WP_Http;
-	$result = $request->request( $url );
+	$result  = $request->request( $url );
 
 	if ( check_result( $result ) ) {
 		$content = $result['body'];
@@ -64,37 +65,37 @@ function get_meta_og_data( $url ) {
 			$data = array();
 
 			$html = new DOMDocument();
-			@$html->loadHTML($html_content);
+			@$html->loadHTML( $html_content );
 
-			$data['title'] = '';
+			$data['title']       = '';
 			$data['description'] = '';
-			$data['img'] = '';
-			$data['date'] = '';
-			$i = 0;
+			$data['img']         = '';
+			$data['date']        = '';
+			$i                   = 0;
 
-			foreach( $html->getElementsByTagName('meta') as $meta ) {
+			foreach ( $html->getElementsByTagName( 'meta' ) as $meta ) {
 
-				if( $meta->getAttribute('property')=='og:title' ) {
-					$data['title'] = $meta->getAttribute('content');
+				if ( $meta->getAttribute( 'property' ) == 'og:title' ) {
+					$data['title'] = $meta->getAttribute( 'content' );
 				}
 
-				if( $meta->getAttribute('property')=='og:description' ) {
-					$data['description'] = $meta->getAttribute('content');
+				if ( $meta->getAttribute( 'property' ) == 'og:description' ) {
+					$data['description'] = $meta->getAttribute( 'content' );
 				}
 
-				if( $meta->getAttribute('property')=='og:image' ) {
-					$data['img'][$i] = $meta->getAttribute('content');
-					$i++;
+				if ( $meta->getAttribute( 'property' ) == 'og:image' ) {
+					$data['img'][ $i ] = $meta->getAttribute( 'content' );
+					$i ++;
 				}
 
-				if (strpos($url, 'eventbrite') !== false) {
-					if( $meta->getAttribute('property')=='event:start_time' ) {
-						$data['date'] = $meta->getAttribute('content');
+				if ( strpos( $url, 'eventbrite' ) !== false ) {
+					if ( $meta->getAttribute( 'property' ) == 'event:start_time' ) {
+						$data['date'] = $meta->getAttribute( 'content' );
 					}
 				}
 			}
 
-			if (isset($data['img'][0]) == false) {
+			if ( isset( $data['img'][0] ) == false ) {
 				$meta_og_img[0] = '';
 			}
 
@@ -107,7 +108,7 @@ function get_meta_og_data( $url ) {
 
 function get_meta_og_on_save( $post_id ) {
 
-	$template_file = get_post_meta( $post_id, '_wp_page_template' ,true );
+	$template_file = get_post_meta( $post_id, '_wp_page_template', true );
 
 	if ( $template_file == 'page-home.php' ) {
 
@@ -119,10 +120,10 @@ function get_meta_og_on_save( $post_id ) {
 
 			if ( $current ) {
 				if ( $data['home_banner_url'] !== $current ) {
-					$data['home_banner_title'] = '';
+					$data['home_banner_title']   = '';
 					$data['home_banner_excerpt'] = '';
-					$data['home_banner_img'] = '';
-					$data['home_banner_expire'] = '';
+					$data['home_banner_img']     = '';
+					$data['home_banner_expire']  = '';
 					update_post_meta( $post_id, 'home_banner_url_old', $data['home_banner_url'] );
 				}
 			} else {
@@ -131,47 +132,47 @@ function get_meta_og_on_save( $post_id ) {
 
 			$og = get_meta_og_data( $data['home_banner_url'] );
 
-			if ( trim($data['home_banner_title']) == '' ) {
-				$_POST['home_banner_title'] = esc_attr($og['title']);
+			if ( trim( $data['home_banner_title'] ) == '' ) {
+				$_POST['home_banner_title'] = esc_attr( $og['title'] );
 			}
-			if ( trim($data['home_banner_excerpt']) == '' ) {
-				$_POST['home_banner_excerpt'] = esc_attr($og['description']);
+			if ( trim( $data['home_banner_excerpt'] ) == '' ) {
+				$_POST['home_banner_excerpt'] = esc_attr( $og['description'] );
 			}
-			if ( trim($data['home_banner_img']) == '' ) {
-				$_POST['home_banner_img'] = esc_attr($og['img'][0]);
+			if ( trim( $data['home_banner_img'] ) == '' ) {
+				$_POST['home_banner_img'] = esc_attr( $og['img'][0] );
 			}
-			if ( isset($og['date']) && strpos($data['home_banner_url'], 'eventbrite') !== false ) {
-				if ( trim($data['home_banner_expire']) == '' ) {
-					$date = esc_attr($og['date']);
-					$date = date('Y-m-d', strtotime($date));
+			if ( isset( $og['date'] ) && strpos( $data['home_banner_url'], 'eventbrite' ) !== false ) {
+				if ( trim( $data['home_banner_expire'] ) == '' ) {
+					$date                        = esc_attr( $og['date'] );
+					$date                        = date( 'Y-m-d', strtotime( $date ) );
 					$_POST['home_banner_expire'] = $date;
 				}
 			}
 		}
 
-		for ( $i=1 ; $i<=6 ; $i++ ) {
+		for ( $i = 1; $i <= 6; $i ++ ) {
 
-			if ( $data['home_card_url_'.$i] ) {
+			if ( $data[ 'home_card_url_' . $i ] ) {
 
-				$current = get_post_meta( $post_id, 'home_card_url_old_'.$i, true );
+				$current = get_post_meta( $post_id, 'home_card_url_old_' . $i, true );
 
 				if ( $current ) {
-					if ( $data['home_card_url_'.$i] !== $current ) {
-						$data['home_card_title_'.$i] = '';
-						$data['home_card_excerpt_'.$i] = '';
-						$data['home_card_img_'.$i] = '';
-						$data['home_card_expire_'.$i] = '';
-						update_post_meta( $post_id, 'home_card_url_old_'.$i, $data['home_card_url_'.$i] );
+					if ( $data[ 'home_card_url_' . $i ] !== $current ) {
+						$data[ 'home_card_title_' . $i ]   = '';
+						$data[ 'home_card_excerpt_' . $i ] = '';
+						$data[ 'home_card_img_' . $i ]     = '';
+						$data[ 'home_card_expire_' . $i ]  = '';
+						update_post_meta( $post_id, 'home_card_url_old_' . $i, $data[ 'home_card_url_' . $i ] );
 					}
 				} else {
-					add_post_meta( $post_id, 'home_card_url_old_'.$i, $data['home_card_url_'.$i], true );
+					add_post_meta( $post_id, 'home_card_url_old_' . $i, $data[ 'home_card_url_' . $i ], true );
 				}
 
-				if (    trim($data['home_card_title_'.$i]) == '' ||
-				        trim($data['home_card_excerpt_'.$i]) == '' ||
-				        trim($data['home_card_img_'.$i]) == '' ||
-				        trim($data['home_card_expire_'.$i]) == '' )
-				{
+				if ( trim( $data[ 'home_card_title_' . $i ] ) == '' ||
+				     trim( $data[ 'home_card_excerpt_' . $i ] ) == '' ||
+				     trim( $data[ 'home_card_img_' . $i ] ) == '' ||
+				     trim( $data[ 'home_card_expire_' . $i ] ) == ''
+				) {
 
 					$og = get_meta_og_data( $data[ 'home_card_url_' . $i ] );
 
@@ -190,6 +191,8 @@ function get_meta_og_on_save( $post_id ) {
 							$date                              = date( 'Y-m-d', strtotime( $date ) );
 							$_POST[ 'home_card_expire_' . $i ] = $date;
 						}
+					} else {
+						$_POST[ 'home_card_expire_' . $i ] = $data[ 'home_card_expire_' . $i ];
 					}
 				}
 			}
