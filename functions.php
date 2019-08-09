6a517eaf32dfb8f81bd5704d88156ebaf3b09bbc
domingobishop <domingobishop@gmail.com>
2019-08-09 17:41:58 +0100

    move get_meta_og_on_save

--- functions.php
+++ functions.php
@@ -23,7 +23,6 @@ add_action( 'save_post', 'check_cards' );
 add_action( 'admin_notices', 'cards_admin_notice' );
 add_action( 'admin_enqueue_scripts', 'admin_style' );
 add_action( 'admin_menu', 'tna_homepage_menu' );
-add_action( 'save_post', 'get_meta_og_on_save' );
 add_action( 'wp_head', 'render_schema' );
 
 // add_filter
